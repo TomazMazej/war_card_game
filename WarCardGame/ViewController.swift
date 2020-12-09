2ebcfd069ed535e84520d10faa1f9e501660e01b
Tomaz Mazej <tomaz.mazej@gmail.com>
2020-12-09 19:22:29 +0100

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Tomaz Mazej on 28/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func dealTapped(_ sender: Any) {
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber{
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber{
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+    }
+}
+
